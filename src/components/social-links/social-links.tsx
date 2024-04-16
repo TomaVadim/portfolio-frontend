ac8031ab163cb0559a69068d2d39bb3e0b6dd3e1
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-04-16 16:03:41 +0300

    feat: create social links component

---
+++ src/components/social-links/social-links.tsx
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition, faGithub, faInstagram, faLinkedin, faTelegram } from "@fortawesome/free-brands-svg-icons";
+
+interface Link {
+  href: string;
+  icon: IconDefinition;
+}
+
+const links: Link[] = [
+  {
+    href: "https://www.linkedin.com/in/vadim-toma-046925247",
+    icon: faLinkedin,
+  },
+  {
+    href: "https://t.me/vadimtoma",
+    icon: faTelegram,
+  },
+  {
+    href: "https://github.com/TomaVadim",
+    icon: faGithub,
+  },
+  {
+    href: "#",
+    icon: faInstagram,
+  },
+];
+
+export const SocialLinks = (): JSX.Element => {
+  return (
+    <ul className="mb-5 flex gap-10">
+      {links.map(({ href, icon }) => (
+        <li>
+          <a href={href} target="_blank">
+            <FontAwesomeIcon className="text-dark w-8 hover:text-primary transition-colors duration-200" icon={icon} />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
