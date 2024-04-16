d64f0ea52e2527f8672a45933eacdebe6166dbba
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-04-16 16:03:55 +0300

    feat: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { RecentPosts } from "@/components/recent-posts/recent-posts";
+import { Introduce } from "@/components/introduce/introduce";
+import { worksList } from "@/components/works/works-list";
+import { PreviewWorks } from "@/components/preview-works/preview-works";
+
 export default function Home() {
-  return <main>Main</main>;
+  return (
+    <>
+      <Introduce />
+      <RecentPosts />
+      <PreviewWorks worksList={worksList} />
+    </>
+  );
 }
