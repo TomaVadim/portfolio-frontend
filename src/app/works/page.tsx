878e082d8f77e62cc0807f351abbdc4bf7735765
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-04-17 12:58:20 +0300

    chore: change styles

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Works() {
   return (
     <section className="content-container pt-20 lg:pt-[160px] tracking-wide">
-      <h1 className="lg:px-[16px] text-dark [font-size:_clamp(1.2rem,5vw,3rem)] font-bold">Works</h1>
+      <h1 className="lg:px-[16px] text-dark [font-size:_clamp(1.5rem,5vw,3rem)] font-bold">Works</h1>
 
       <div className="-mx-[16px] lg:mx-0 lg:mb-14 lg:mt-6">
         <WorksList worksList={worksList} />
