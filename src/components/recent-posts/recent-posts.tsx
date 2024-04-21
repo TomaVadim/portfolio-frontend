00a0670bd5f48b7f698a867c20c4ba69a3d2b5d7
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-04-21 10:50:57 +0300

    chore: change size languages toggler

--- src/components/recent-posts/recent-posts.tsx
+++ src/components/recent-posts/recent-posts.tsx
@@ -12,7 +12,7 @@ export const RecentPosts = (): JSX.Element => {
   return (
     <section className="bg-secondary/10 tracking-wide">
       <div className="pb-8 content-container">
-        <div className="py-6 w-full flex justify-between">
+        <div className="py-6 w-full flex justify-between items-center">
           <h6 className="text-[22px] text-dark">{t("latest-updates-title")}</h6>
           <Link href="/blog" className="text-secondary hover:text-primary transition-colors duration-200">
             {t("button-view-all")}
